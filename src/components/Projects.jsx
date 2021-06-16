533d7b4f4d8f341de3be6bebe5962aaa5f497953
charlenebatiste <batistecharlene@gmail.com>
2021-06-16 10:26:51 -0400

    formatting project and skills component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,353 +1,274 @@
-// import React from "react";
-// import {
-// 	makeStyles,
-// 	Card,
-// 	CardActionArea,
-// 	CardActions,
-// 	CardContent,
-// 	CardMedia,
-// 	Button,
-// 	Typography,
-// 	Paper,
-// 	Grid,
-// } from "@material-ui/core";
-
-// const useStyles = makeStyles((theme) => ({
-// 	cardroot: {
-// 		// maxWidth: 345,
-// 	},
-// 	media: {
-// 		height: 140,
-// 	},
-// 	root: {
-// 		// flexGrow: 1,
-// 	},
-// 	paper: {
-// 		// padding: theme.spacing(1),
-// 	},
-// }));
-
-// const Projects = () => {
-// 	const classes = useStyles();
-
-// 	function FormRow() {
-// 		return (
-// 			<React.Fragment>
-// 				<Grid item xs={4}>
-// 					<Paper
-// 						className={classes.paper}
-// 					>
-// 						<Card
-// 							className={
-// 								classes.cardroot
-// 							}
-// 						>
-// 							<CardActionArea>
-// 								<CardMedia
-// 									className={
-// 										classes.media
-// 									}
-// 									image=""
-// 									title="Contemplative Reptile"
-// 								/>
-// 								<CardContent>
-// 									<Typography
-// 										gutterBottom
-// 										variant="h5"
-// 										component="h2"
-// 									>
-// 										Bookhub
-// 									</Typography>
-// 									<Typography
-// 										variant="body2"
-// 										color="textSecondary"
-// 										component="p"
-// 									>
-// 										A
-// 										full-stack
-// 										application
-// 										where book
-// 										lovers can
-// 										search
-// 										books to
-// 										add to
-// 										their
-// 										Bookshelf,
-// 										create
-// 										posts and
-// 										interact
-// 										with other
-// 										users.
-// 									</Typography>
-// 								</CardContent>
-// 							</CardActionArea>
-// 							<CardActions>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									See Repo
-// 								</Button>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									Visit Live
-// 									Site
-// 								</Button>
-// 							</CardActions>
-// 						</Card>
-// 					</Paper>
-// 				</Grid>
-// 				<Grid item xs={4}>
-// 					<Paper
-// 						className={classes.paper}
-// 					>
-// 						<Card
-// 							className={
-// 								classes.cardroot
-// 							}
-// 						>
-// 							<CardActionArea>
-// 								<CardMedia
-// 									className={
-// 										classes.media
-// 									}
-// 									image=""
-// 									title="Contemplative Reptile"
-// 								/>
-// 								<CardContent>
-// 									<Typography
-// 										gutterBottom
-// 										variant="h5"
-// 										component="h2"
-// 									>
-// 										The
-// 										Healthful
-// 										Pantry
-// 									</Typography>
-// 									<Typography
-// 										variant="body2"
-// 										color="textSecondary"
-// 										component="p"
-// 									>
-// 										The
-// 										Healthful
-// 										Pantry is
-// 										a
-// 										fullstack
-// 										MERN app
-// 										that
-// 										strives to
-// 										provide
-// 										users with
-// 										food
-// 										allergies,
-// 										intolerances
-// 										and
-// 										restrivcive
-// 										diets a
-// 										single
-// 										place to
-// 										find
-// 										recipes
-// 										that fit
-// 										their
-// 										nutritional
-// 										needs.
-// 									</Typography>
-// 								</CardContent>
-// 							</CardActionArea>
-// 							<CardActions>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									See Repo
-// 								</Button>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									Visit Live
-// 									Site
-// 								</Button>
-// 							</CardActions>
-// 						</Card>
-// 					</Paper>
-// 				</Grid>
-// 				<Grid item xs={4}>
-// 					<Paper
-// 						className={classes.paper}
-// 					>
-// 						<Card
-// 							className={
-// 								classes.cardroot
-// 							}
-// 						>
-// 							<CardActionArea>
-// 								<CardMedia
-// 									className={
-// 										classes.media
-// 									}
-// 									image=""
-// 									title="Contemplative Reptile"
-// 								/>
-// 								<CardContent>
-// 									<Typography
-// 										gutterBottom
-// 										variant="h5"
-// 										component="h2"
-// 									>
-// 										Floyd's
-// 										Good Dog
-// 									</Typography>
-// 									<Typography
-// 										variant="body2"
-// 										color="textSecondary"
-// 										component="p"
-// 									>
-// 										A website
-// 										built for
-// 										a small
-// 										business
-// 										in
-// 										Grovetown,
-// 										GA.
-// 										Floyd's
-// 										Good Dog
-// 										is a fully
-// 										responsive
-// 										website
-// 										deisign to
-// 										inform and
-// 										generate
-// 										clients
-// 										for a
-// 										family-owned
-// 										doggy
-// 										daycare
-// 									</Typography>
-// 								</CardContent>
-// 							</CardActionArea>
-// 							<CardActions>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									See Repo
-// 								</Button>
-// 								<Button
-// 									size="small"
-// 									color="primary"
-// 								>
-// 									Visit Live
-// 									Site
-// 								</Button>
-// 							</CardActions>
-// 						</Card>
-// 					</Paper>
-// 				</Grid>
-// 			</React.Fragment>
-// 		);
-// 	}
-
-// 	return (
-// 		<div className={classes.root}>
-// 			<Grid container spacing={1}>
-// 				<Grid
-// 					container
-// 					item
-// 					xs={12}
-// 					spacing={3}
-// 				>
-// 					<FormRow />
-// 				</Grid>
-// 			</Grid>
-// 		</div>
-// 	);
-// };
-
-// export default Projects;
-
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import IconButton from "@material-ui/core/IconButton";
-import StarBorderIcon from "@material-ui/icons/StarBorder";
-import tileData from "./tileData";
+import {
+	makeStyles,
+	Button,
+	Grid,
+	Paper,
+	Typography,
+	ButtonBase,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		display: "flex",
-		flexWrap: "wrap",
-		justifyContent: "space-around",
-		overflow: "hidden",
-		backgroundColor:
-			theme.palette.background.paper,
+		flexGrow: 1,
 	},
-	gridList: {
-		flexWrap: "nowrap",
-		// Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-		transform: "translateZ(0)",
+	paper: {
+		padding: theme.spacing(2),
+		margin: "auto",
+		maxWidth: 500,
 	},
-	title: {
-		color: theme.palette.primary.light,
+	image: {
+		width: 128,
+		height: 128,
 	},
-	titleBar: {
-		background:
-			"linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+	img: {
+		margin: "auto",
+		display: "block",
+		maxWidth: "100%",
+		maxHeight: "100%",
 	},
 }));
 
-// import image from "path/to/image.jpg";
-
-const tileData = [
-	{
-		// img: image,
-		title: "Image",
-		author: "author",
-	},
-];
-
-export default function Projects() {
+export default function ComplexGrid() {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
-			<GridList
-				className={classes.gridList}
-				cols={2.5}
-			>
-				{tileData.map((tile) => (
-					<GridListTile key={tile.img}>
-						<img
-							src={tile.img}
-							alt={tile.title}
-						/>
-						<GridListTileBar
-							title={tile.title}
-							classes={{
-								root: classes.titleBar,
-								title: classes.title,
-							}}
-							actionIcon={
-								<IconButton
-									aria-label={`star ${tile.title}`}
-								>
-									<StarBorderIcon
-										className={
-											classes.title
-										}
-									/>
-								</IconButton>
-							}
-						/>
-					</GridListTile>
-				))}
-			</GridList>
-		</div>
+		<section className="ProjectWrapper">
+			<div className={classes.root}>
+				<Paper className={classes.paper}>
+					<Grid container spacing={2}>
+						<Grid item>
+							<ButtonBase
+								className={
+									classes.image
+								}
+							>
+								<img
+									className={
+										classes.img
+									}
+									alt="complex"
+									src="/static/images/grid/complex.jpg"
+								/>
+							</ButtonBase>
+						</Grid>
+						<Grid item sm container>
+							<Grid
+								item
+								xs
+								container
+								direction="column"
+								spacing={2}
+							>
+								<Grid item xs>
+									<Typography
+										gutterBottom
+										variant="subtitle1"
+									>
+										Bookhub
+									</Typography>
+									<Typography
+										variant="body2"
+										gutterBottom
+									>
+										A
+										full-stack
+										application
+										where book
+										lovers can
+										search
+										books to
+										add to
+										their
+										Bookshelf,
+										create
+										posts and
+										interact
+										with other
+										users.
+									</Typography>
+								</Grid>
+								<Grid item>
+									<Button
+										size="small"
+										color="primary"
+									>
+										See Repo
+									</Button>
+									<Button
+										size="small"
+										color="primary"
+									>
+										Visit Live
+										Site
+									</Button>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Paper>
+				<Paper className={classes.paper}>
+					<Grid container spacing={2}>
+						<Grid item>
+							<ButtonBase
+								className={
+									classes.image
+								}
+							>
+								<img
+									className={
+										classes.img
+									}
+									alt="complex"
+									src="/static/images/grid/complex.jpg"
+								/>
+							</ButtonBase>
+						</Grid>
+						<Grid item sm container>
+							<Grid
+								item
+								xs
+								container
+								direction="column"
+								spacing={2}
+							>
+								<Grid item xs>
+									<Typography
+										gutterBottom
+										variant="subtitle1"
+									>
+										The
+										Healthful
+										Pantry
+									</Typography>
+									<Typography
+										variant="body2"
+										gutterBottom
+									>
+										The
+										Healthful
+										Pantry is
+										a
+										fullstack
+										MERN app
+										that
+										strives to
+										provide
+										users with
+										food
+										allergies,
+										intolerances
+										and
+										restrivcive
+										diets a
+										single
+										place to
+										find
+										recipes
+										that fit
+										their
+										nutritional
+										needs.
+									</Typography>
+								</Grid>
+								<Grid item>
+									<Button
+										size="small"
+										color="primary"
+									>
+										See Repo
+									</Button>
+									<Button
+										size="small"
+										color="primary"
+									>
+										Visit Live
+										Site
+									</Button>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Paper>
+				<Paper className={classes.paper}>
+					<Grid container spacing={2}>
+						<Grid item>
+							<ButtonBase
+								className={
+									classes.image
+								}
+							>
+								<img
+									className={
+										classes.img
+									}
+									alt="complex"
+									src="/static/images/grid/complex.jpg"
+								/>
+							</ButtonBase>
+						</Grid>
+						<Grid item sm container>
+							<Grid
+								item
+								xs
+								container
+								direction="column"
+								spacing={2}
+							>
+								<Grid item xs>
+									<Typography
+										gutterBottom
+										variant="subtitle1"
+									>
+										Floyd's
+										God Dog
+									</Typography>
+									<Typography
+										variant="body2"
+										gutterBottom
+									>
+										A website
+										built for
+										a small
+										business
+										in
+										Grovetown,
+										GA.
+										Floyd's
+										Good Dog
+										is a fully
+										responsive
+										website
+										deisign to
+										inform and
+										generate
+										clients
+										for a
+										family-owned
+										doggy
+										daycare
+									</Typography>
+								</Grid>
+								<Grid item>
+									<Button
+										size="small"
+										color="primary"
+									>
+										See Repo
+									</Button>
+									<Button
+										size="small"
+										color="primary"
+									>
+										Visit Live
+										Site
+									</Button>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Paper>
+			</div>
+		</section>
 	);
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,32 +1,238 @@
-import React from "react";
 import "./css/Skill.css";
 
-const Skill = () => {
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+	},
+}));
+
+export default function Skills() {
+	const classes = useStyles();
+
 	return (
-		<section className="skillWrapper">
-			<h1>Skills</h1>
-			<div>
-				Programming Languages: HTML, CSS3,
-				Javascript
-			</div>
-			<div>
-				Libraries and Frameworks:
-				Bootstrap, Node, Express, React,
-				Sequelize, Mongoose
-			</div>
-			<div>
-				Databases: Postgres, SQL, MongoDB
-			</div>
-			<div>
-				{" "}
-				Additional Skills: Git, Github,
-				Slack, Responsive Design,
-				Wireframes, Balsamiq, LucidChart,
-				Object-Oriented Programming, AJAX,
-				REST
+		<section className="skillsWrapper">
+			<div className={classes.root}>
+				<Grid container spacing={3}>
+					<Grid item xs={12}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Skills
+						</Paper>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							xs=12 sm=6
+						</Paper>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							xs=12 sm=6
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							HTML
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Javascript
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							CSS
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Bootstrap
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Materialize
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							NodeJS
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Express
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							React
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							SQL
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							MongoDB
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Git
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Github
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Slack
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Responsive Design
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Wireframes
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Balsamiq
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							LucidChart
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							Object-Oriented
+							Programming
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							AJAX
+						</Paper>
+					</Grid>
+					<Grid item xs={6} sm={3}>
+						<Paper
+							className={
+								classes.paper
+							}
+						>
+							RESTful Programming
+						</Paper>
+					</Grid>
+				</Grid>
 			</div>
 		</section>
 	);
-};
-
-export default Skill;
+}
