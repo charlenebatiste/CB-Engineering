f0f31c86a4518427c14ae5c49c8c57d61b183aba
charlenebatiste <batistecharlene@gmail.com>
2021-06-17 19:19:38 -0400

    work on flex and image formatting in about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,77 +3,60 @@ import headshot from "./assets/headshot.JPG";
 import React from "react";
 
 import {
-	makeStyles,
-	Card,
-	CardContent,
-	CardMedia,
+	Grid,
 	Typography,
+	CardMedia,
 } from "@material-ui/core/";
 
-const useStyles = makeStyles(() => ({
-	root: {
-		display: "flex",
-	},
-	details: {
-		display: "flex",
-		flexDirection: "column",
-	},
-	cover: {
-		width: "50vw",
-	},
-}));
-
 export default function About() {
-	const classes = useStyles();
-
 	return (
-		<div className="AboutSection">
-			<Card className={classes.root}>
-				<div className={classes.details}>
-					<CardContent
-						className={
-							classes.content
-						}
-						id="cardWrapper"
+		<div
+			className="AboutSection"
+			id="cardWrapper"
+		>
+			<div className="about__header">
+				<Typography
+					component="h4"
+					variant="h4"
+				>
+					About Me
+				</Typography>
+			</div>
+			<Grid container>
+				<Grid item xs={12} sm={6} md={4}>
+					<CardMedia
+						component="img"
+						image={headshot}
+					/>
+					{/* <img src={headshot} /> */}
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<Typography
+						className="card__text"
+						component="p"
 					>
-						<Typography
-							component="h4"
-							variant="h4"
-						>
-							About Me
-						</Typography>
-						<Typography className="card__text">
-							I am a full-stack
-							software engineer with
-							a passion for creating
-							responsive products
-							and clean designs
-							oriented around the
-							user experience. I
-							thrive in creative,
-							team-centric
-							environments where my
-							previous work as a 911
-							Communications Officer
-							lends to a balanced
-							approach to
-							challenging
-							situations. From my
-							ability to determine
-							problems to adjusting
-							plans quickly I strive
-							to develop elegant
-							solutions out of
-							stressful situations.
-						</Typography>
-					</CardContent>
-				</div>
-				<CardMedia
-					className={classes.cover}
-					image={headshot}
-					title="Live from space album cover"
-				/>
-			</Card>
+						I am a full-stack software
+						engineer with a passion
+						for creating responsive
+						products and clean designs
+						oriented around the user
+						experience. I thrive in
+						creative, team-centric
+						environments where my
+						previous work as a 911
+						Communications Officer
+						lends to a balanced
+						approach to challenging
+						situations. From my
+						ability to determine
+						problems to adjusting
+						plans quickly I strive to
+						develop elegant solutions
+						out of stressful
+						situations.
+					</Typography>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
