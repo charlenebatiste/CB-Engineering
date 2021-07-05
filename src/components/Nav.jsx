cefd4e8f58f5b96fee792242a9245011e5f294e3
charlenebatiste <batistecharlene@gmail.com>
2021-07-05 10:23:35 -0400

    resolved justification errors on navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,8 +11,7 @@ const Nav = () => {
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <div class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div class="collapse navbar-collapse navbar-links" id="navbarTogglerDemo01">
                             <div>
                                 <Link
                                     className="navLink hvr-grow nav-item"
@@ -57,9 +56,8 @@ const Nav = () => {
                                     to="#contact"
                                 >
                                     Contact Me
-                                </Link>{" "}
+                                </Link>
                             </div>
-                        </div>
                     </div>
                 </div>
             </nav>
